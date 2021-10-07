bf8edcf5392ed6c39c3088c04881688d77913581
codervinicius <vinicaixx@gmail.com>
2021-10-07 08:58:52 -0300

    att

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apis = {
-  development: "http://localhost:4000/api",
-  production: "https://ironbank-server.herokuapp.com/api",
+  development: "http://localhost:4000/",
+  production: "https://cadastro-server.herokuapp.com/",
 };
 
 const api = axios.create({
